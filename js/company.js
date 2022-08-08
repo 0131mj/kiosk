ff72fc0331e84008b8607d96308932d8f72414b3
mjbae <0131mj@gmail.com>
2022-08-09 04:44:03 +0900

    - 1차 작업 대략 완료

--- js/company.js
+++ js/company.js
@@ -13,7 +13,7 @@ const companyMenu = {
         subMenu: {
             history: "연혁",
             vision: "비전",
-            introduce: "소개",
+            office: "사업소 소개",
             beauty: "아름다운 경남",
         }
     },
@@ -50,10 +50,36 @@ companyNav.outerHTML = `
 </div>
 `;
 
-function loadHTML(){
-    fetch(`./company_content/${curMenu}_${curSubMenu}.html`)
-        .then(response=> response.text())
-        .then(text=> document.getElementById('company-content').innerHTML = text);
+if (curMenu === "hq" && curSubMenu === "history") {
+    const hqHistory = [
+        ["1910.6.9", "한일와사㈜ 마산지점", ""],
+        ["1947.10.1", "남선전기㈜ 마산지점", "01"],
+        ["1961.7.1", "한국전력㈜ 경남지점 마산영업소", "02"],
+        ["1982.1.1", "한국전력공사 경남지사","03"],
+        ["1988.11.2", "한국전력공사 창원전력관리처 분리발족", "04"],
+        ["1988.11.2", "한국전력공사 경남사업본부", "05"],
+        ["2009.1.12", "판매 및 송변전 통합 경남지역본부", "06"],
+        ["2018.12.27~", "한국전력공사 경남본부"],
+    ]
+    document.getElementById('company-content').innerHTML = `<ol class="hq-history">${
+        hqHistory.reduce((acc, [date, text, file])=>{
+            acc += `<li>
+                        <div class="hq-history-date">${date}</div>
+                        <div class="hq-history-content">
+                            ${text}
+                            ${file ? `<img src="../img/hq_history_${file}.png" alt="${text}">`: ""}                            
+                        </div>
+                    </li>`;
+            return acc;
+        },"")
+    }</ol>`
+} else {
+    function loadHTML() {
+        fetch(`./company_content/${curMenu}_${curSubMenu}.html`)
+            .then(response => response.text())
+            .then(text => document.getElementById('company-content').innerHTML = text);
+    }
+
+    loadHTML();
 }
 
-loadHTML();
