3229a37d16ff0e3ec026b8a599e7dee7c96df5cf
mjbae <0131mj@gmail.com>
2022-08-09 10:20:18 +0900

    - 컨트롤 메뉴 폰트 크기 확대 및 명칭 변경 (HOME => 홈) - 홈 화면에서 헤더 타이틀 삭제

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const controlMenus = `
 <div class="control-menus">
-    <a class="control-menu" href="./index.html">HOME</a>
-    <button class="control-menu" id="back-btn">뒤로 가기</button>
+    <a class="control-menu" href="./index.html">홈</a>
+    <button class="control-menu" id="back-btn">뒤로<br/>가기</button>
     <button class="control-menu global-menu-open-btn">메뉴</button>
 </div>
 `;
