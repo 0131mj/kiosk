b835f182fdbb408b08facc5a1935334302671473
mjbae <0131mj@gmail.com>
2022-08-15 13:54:56 +0900

    - 홈화면이 아닌 곳에서는 컨트롤 메뉴 접힌 상태로 표시

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const controlMenus = `
 <div class="control-menus">
+    <button id="control-spread-btn">
+    <svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+    </svg>
+</button>
     <a class="control-menu" href="./index.html">홈</a>
     <button class="control-menu global-menu-open-btn">전체<br/>메뉴</button>
 </div>
@@ -26,19 +31,29 @@ const renderControlMenu = () => {
     if (controlMenusEl) {
         controlMenusEl.outerHTML = controlMenus;
         document.querySelectorAll('a').forEach(a => {
-            a.addEventListener("click", (e)=>{
+            a.addEventListener("click", (e) => {
                 e.stopPropagation()
             })
         })
         document.querySelectorAll('button').forEach(a => {
-            a.addEventListener("click", (e)=>{
+            a.addEventListener("click", (e) => {
                 e.stopPropagation()
             })
         })
         const globalMenuOpenBtnEl = document.querySelector(".global-menu-open-btn");
         globalMenuOpenBtnEl.addEventListener("click", showGlobalMenu);
-        const backBtnEl = document.getElementById("back-btn");
-        backBtnEl.addEventListener("click", moveBack);
+
+        /** 홈 메뉴 아닐 때는 접어서 표시 **/
+        const isHome = window.location.href.endsWith('index.html');
+        if (!isHome) {
+            const controlSpreadBtn = document.getElementById("control-spread-btn");
+            const el = document.querySelector(".control-menus");
+            el.classList.add("fold")
+            controlSpreadBtn.addEventListener("click", (e) => {
+                e.stopPropagation();
+                el.classList.remove("fold")
+            })
+        }
     }
 }
 
@@ -60,12 +75,12 @@ const globalMenus = `
     <div class="global-menu-panel">          
         <div class="global-menus">
             ${Object.entries(globalMenuObj).reduce(
-                (rows, [link, text]) => rows + `
+    (rows, [link, text]) => rows + `
                     <a href="${link}.html" class="global-menu">
                         ${text}
                     </a>`, ""
-                )
-            }
+)
+}
         </div>    
         <footer class="global-menu-close-btn">닫기</footer>    
     </div>
@@ -92,15 +107,17 @@ headerHomeBtn.innerText = "홈";
 headerHomeBtn.classList.add("header-control-btn");
 headerHomeBtn.classList.add("header-home-btn");
 headerHomeBtn.setAttribute("href", "./index.html")
-headerHomeBtn.addEventListener("click", (e)=>{
+headerHomeBtn.addEventListener("click", (e) => {
     e.stopPropagation();
 })
-
-titleHeader.insertBefore(headerHomeBtn, titleHeader.firstChild);
 
 const headerBackBtn = document.createElement("button");
 headerBackBtn.innerText = "뒤로 가기";
 headerBackBtn.classList.add("header-control-btn");
 headerBackBtn.classList.add("header-back-btn");
 headerBackBtn.addEventListener("click", moveBack);
-titleHeader.appendChild(headerBackBtn);
+
+if (titleHeader) {
+    titleHeader.insertBefore(headerHomeBtn, titleHeader.firstChild);
+    titleHeader.appendChild(headerBackBtn);
+}
